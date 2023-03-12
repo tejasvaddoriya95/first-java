331828afdcafb5a6355ba2928d1c36b364864e56
tejas <tejasvaddoriya95@gmail.com>
2023-03-12 17:50:27 +0000

    first java commit

---
+++ src/MyDetails.java
@@ -0,0 +1,32 @@
+public class MyDetails {
+
+    static int age = 26;
+    static String name = "Tejas";
+    static int mobilenumber = 777682345;
+    static int rollnumber = 34;
+    static String address = "144 camden high st camden NW1 7PG";
+    static String grade = "A";
+    static String eligibility = "true";
+    static double amount = 28.04;
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(mobilenumber);
+        System.out.println(rollnumber);
+        System.out.println(address);
+        System.out.println(grade);
+        System.out.println(eligibility);
+        System.out.println(amount);
+
+    }
+
+
+
+
+
+
+}
